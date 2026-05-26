c145bd14b51a8d1daaaaea8c3171862364884595
giorgostt97 <klevoitems@gmail.com>
2026-05-26 21:26:59 +0300

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -2,25 +2,35 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const basicAuth = request.headers.get("authorization");
-
   if (request.nextUrl.pathname.startsWith("/admin")) {
-    if (basicAuth) {
-      const authValue = basicAuth.split(" ")[1];
-      const [user, pwd] = atob(authValue).split(":");
+    const authHeader = request.headers.get("authorization");
 
-      if (user === "admin" && pwd === "metin2") {
+    if (authHeader) {
+      const encoded = authHeader.split(" ")[1];
+      const decoded = atob(encoded);
+
+      const [user, password] = decoded.split(":");
+
+      if (
+        user === "admin" &&
+        password === "metin2"
+      ) {
         return NextResponse.next();
       }
     }
 
-    return new NextResponse("Auth required", {
+    return new NextResponse("Authentication required", {
       status: 401,
       headers: {
-        "WWW-Authenticate": 'Basic realm="Secure Area"',
+        "WWW-Authenticate":
+          'Basic realm="Secure Area"',
       },
     });
   }
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
